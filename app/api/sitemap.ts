72bc42ef16b25c0b0b485a071aee9f2748d57ecb
Aswin-optiblack <aswin@optiblack.com>
2025-02-11 13:27:22 +0530

    fix: sitemap API

--- app/api/sitemap.ts
+++ app/api/sitemap.ts
@@ -1,16 +1,22 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest } from "next/server";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.setHeader("Content-Type", "application/xml");
-  res.write(`<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      <url>
-        <loc>https://askmebot.aswinkrishna.com/</loc>
-        <lastmod>${new Date().toISOString()}</lastmod>
-        <changefreq>daily</changefreq>
-        <priority>1.0</priority>
-      </url>
-    </urlset>
-  `);
-  res.end();
+export const runtime = "edge"; 
+
+export async function GET(req: NextRequest) {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>https://askmebot.aswinkrishna.com/</loc>
+      <lastmod>${new Date().toISOString()}</lastmod>
+      <changefreq>daily</changefreq>
+      <priority>1.0</priority>
+    </url>
+  </urlset>`;
+
+  return new Response(sitemap, {
+    headers: {
+      "Content-Type": "application/xml",
+      "Cache-Control": "no-store"
+    },
+  });
 }
